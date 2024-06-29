45f1626af6a3e520a8fc99e64ff0edbadf7f1e5d
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2024-06-29 18:31:57 -0400

    init

---
+++ src/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QM_EnableConsole
+{
+    [BepInPlugin("bnk_redspy.QM_EnableConsole", "QM_EnableConsole", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        public static BepInEx.Logging.ManualLogSource Log { get; set; }
+
+        private void Awake()
+        {
+            Logger.LogInfo("---------awake ran");
+
+            Log = Logger;
+
+            Harmony harmony = new Harmony("test");
+            harmony.PatchAll();
+
+        }
+    }
+}
---
+++ src/VertTableParser_ParseLine_Patch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QM_EnableConsole
+{
+    [HarmonyPatch(typeof(VertTableParser<MGSC.GlobalSettings>), "ParseLine")]
+    public static class VertTableParser_ParseLine_Patch
+    {
+
+        public static void Prefix(string[] parsed)
+        {
+            try
+            {
+                if (parsed[0] == "Console" && parsed[1] == "false")
+                {
+                    parsed[1] = "true";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log.LogError("ParseLine prefix error: " + ex.ToString());
+            }
+        }
+
+    }
+}
